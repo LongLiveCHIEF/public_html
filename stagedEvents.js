551fe8ee4500937895f2ef735b103189d49a64a0
Brian Vanderbusch <brian@2pioneers.com>
2013-07-18 09:15:34 -0500

    only commit

---
+++ stagedEvents.js
@@ -0,0 +1,19 @@
+var data = {
+    lat:40.458208,
+    long:-88.961535,
+    victimLevel:3
+
+};
+
+addVictim(map,data);
+
+var data1 = {
+    lat:40.458208,
+    long:-88.961535,
+    victimLevel:3
+
+};
+
+addVictim(map,data1);
+
+
